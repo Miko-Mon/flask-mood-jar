5e9835d686def50e76576b5fb6ab170ce8856a0d
Sean Montecillo <montecillomikhail@gmail.com>
2026-06-13 16:38:51 +0800

    refactor: modified the __init__.py file to make it more secure, the secret key was also changed as it was exposed in a previous commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "Nikke"
-
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+    
     return app
