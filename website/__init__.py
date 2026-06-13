ec9ec35650c2fda8d96ad8ca29b1c899fb358ec3
Sean Montecillo <montecillomikhail@gmail.com>
2026-06-13 18:52:34 +0800

    funct: registered the views file into the init file to access the home page

--- website/__init__.py
+++ website/__init__.py
@@ -7,5 +7,9 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+    from .views import views 
+
+    app.register_blueprint(views, url_prefix='/')
     
     return app
