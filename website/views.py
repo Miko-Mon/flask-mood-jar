39120ed3a0e0a845869f99d587fdf40298e9a5d7
Sean Montecillo <montecillomikhail@gmail.com>
2026-06-25 16:21:54 +0800

    ADD: Added get-quotes and add-quotes routes for the views file

--- website/views.py
+++ website/views.py
@@ -1,7 +1,15 @@
-from flask import Blueprint 
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home_page():
-    return "<h1>Hello World!</h1>"
+    return render_template("home.html")
+
+@views.route('/get-quotes')
+def get_quotes():
+    return "<h1>Get Quotes Here!!</h1>"
+
+@views.route('/add-quotes')
+def add_quotes():
+    return "<h1>Add Quotes Here!</h1>"
