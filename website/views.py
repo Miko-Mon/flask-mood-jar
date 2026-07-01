e06ef1d5dc69f99e8232956ff256f0a38e5b75df
Sean Montecillo <montecillomikhail@gmail.com>
2026-07-01 15:21:40 +0800

    refactor: replaced each return statement from plain html to render_template so that proper content can be displayed

--- website/views.py
+++ website/views.py
@@ -4,16 +4,16 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home_page():
-    return render_template("home.html")
+    return render_template("home.html", title="Homepage")
 
 @views.route('/quotes')
 def get_quotes():
-    return "<h1>Get Quotes Here!!</h1>"
+    return render_template("quotes.html", title="Quotes")
 
 @views.route('/mood-jar')
 def mood_jar():
-    return "<h1>Mood Jar Here!!"
+    return render_template("mood_jar.html", title="Mood Jar")
 
 @views.route('/mood-tracker')
 def mood_tracker():
-    return "<h1>Mood Tracker Here!!"
+    return render_template("mood_tracker.html", title="Mood Tracker")
