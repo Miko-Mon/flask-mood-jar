ada5c728946ad514037bbce67ff2689241ceeb62
Sean Montecillo <montecillomikhail@gmail.com>
2026-07-01 14:47:36 +0800

    refactor: revised the views file by removing add-quotes and get-quotes and replacing them with quotes, mood-jar, and mood-tracker

--- website/views.py
+++ website/views.py
@@ -6,10 +6,14 @@ views = Blueprint('views', __name__)
 def home_page():
     return render_template("home.html")
 
-@views.route('/get-quotes')
+@views.route('/quotes')
 def get_quotes():
     return "<h1>Get Quotes Here!!</h1>"
 
-@views.route('/add-quotes')
-def add_quotes():
-    return "<h1>Add Quotes Here!</h1>"
+@views.route('/mood-jar')
+def mood_jar():
+    return "<h1>Mood Jar Here!!"
+
+@views.route('/mood-tracker')
+def mood_tracker():
+    return "<h1>Mood Tracker Here!!"
