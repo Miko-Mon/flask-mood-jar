a44a5a7fa71474c4a3191ce15a01af8bac93cdfd
Sean Montecillo <montecillomikhail@gmail.com>
2026-07-14 15:34:35 +0800

    funct: Added db config

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,31 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+db = SQLAlchemy()
+
 def create_app():
     app = Flask(__name__)
+
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_name = os.getenv("DB_NAME")
+
+    if not all ([db_user, db_password, db_host, db_name]):
+        raise RuntimeError("Missing one or more database environment variables")
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}"
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     from .views import views 
     from .auth import auth
