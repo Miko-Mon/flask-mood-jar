46d6e96359c4fa10f0fbdcfde393df4f802b112e
Sean Montecillo <montecillomikhail@gmail.com>
2026-07-17 22:05:10 +0800

    add: added migrations to the init file

--- heartscape/__init__.py
+++ heartscape/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_migrate import Migrate 
+
+migrate = Migrate()
 
 load_dotenv()
 
@@ -26,6 +29,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .views import views 
     from .auth import auth
