1c26015c036d02ca589f9d59246b0710eb387cb5
Sean Montecillo <montecillomikhail@gmail.com>
2026-07-17 22:06:36 +0800

    add: added the add_user function to add users to the database upon sign up

--- heartscape/auth.py
+++ heartscape/auth.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 import re
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -47,7 +50,10 @@ def sign_up():
             flash("Passwords do not match.", "error")
             return render_template("sign_up.html", title="Sign-up")
 
-        # Database Operation here: Pending
+        # Database Operation: Adding a new user
+        new_user = User(email=email, username=username, password=generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
 
         flash("Account successfully created!", "success")
 
