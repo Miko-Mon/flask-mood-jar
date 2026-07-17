ef6fb0d63c55992fb7064324e79fd4c42d78109c
Sean Montecillo <montecillomikhail@gmail.com>
2026-07-17 22:04:39 +0800

    refactor: increased the column lenght for the password to accomodate its hashed value

--- heartscape/models.py
+++ heartscape/models.py
@@ -14,5 +14,5 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     quotes = db.relationship('Quote')
